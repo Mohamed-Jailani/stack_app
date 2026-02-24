f9dc6593cc6ebdde61ad9cce8b51811ac42bb49f
Mohamed-Jailani <mohamedjailanip@gmail.com>
2026-02-24 13:02:55 +0530

    logo updated in dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -116,6 +116,14 @@ export default function Dashboard() {
           }}
           className="relative z-10 w-full max-w-md bg-white/10 backdrop-blur-2xl border border-white/20 shadow-[0_25px_60px_rgba(0,0,0,0.5)] rounded-3xl p-8 text-white"
         >
+          <div className="flex justify-center mb-2">
+            <img
+              src="/logo.png"
+              alt="Aadhaar Extractor Logo"
+              className="w-28 h-30 object-contain drop-shadow-[0_10px_25px_rgba(168,85,247,0.6)] transition-transform duration-300 hover:scale-110"
+            />
+          </div>
+
           <h2 className="text-2xl font-bold mb-6 text-center">
             Upload Aadhaar
           </h2>
